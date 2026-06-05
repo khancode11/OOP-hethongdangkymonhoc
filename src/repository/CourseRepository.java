8f490ad6ff3012fa6783912c4b6c4cb9670bb567
nghiadao <daominhnghia2005@gmail.com>
2026-06-05 11:59:58 +0700

    them class StudentRepository

--- src/repository/CourseRepository.java
+++ src/repository/CourseRepository.java
@@ -13,14 +13,29 @@ public class CourseRepository {
     //neu courseid da ton tai thi ghi de
      public void save(Course course){
          if (course == null || course.getCourseId() == null){
-             throw new IllegalArgumentException("Course hoac courseid khong duoc null.")
+             throw new IllegalArgumentException("Course hoac co courseid khong duoc null.")
          }
          courseMap.put(course.getCourseId(), course);
      }
 // tim mon hoc theo id
-    public Optional<Course> findById(String courseId){
-         return Optional.ofNullable(courseMap.get(courseId));
+    public Optional<Course> findById(String courseId) {
+        return Optional.ofNullable(courseMap.get(courseId));
 
     }
-    
+//lay tat ca mon hoc
+    public List<Course> findAll(){
+         return new ArrayList<>(courseMap.values());
+    }
+//tim mon hoc theo ten
+    public List<Course> findByName(String name){
+         if (name == null || name.isBlank()) return Collections.emptyList();
+         String keyword = name.trim().toLowerCase();
+         return courseMap.values().stream().filter(c->c.getCourseName() != null && c.getCourseName().toLowerCase().contains(keyword)).collect(Collectors.toList());
+    }
+    public List<Course>findByLecturer(String lecturerId){
+         if (lecturerId == null) return Collections.emptyList();
+         return courseMap.values().stream()
+                 .
+    }
+
 }
