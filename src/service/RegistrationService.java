24baed6a006d4d42b3cd5330e9460d493ecec398
pnj312 <phamnguyen2500@gmail.com>
2026-06-09 08:01:13 +0700

    sửa lại 2 class ở package service

--- src/service/RegistrationService.java
+++ src/service/RegistrationService.java
@@ -1,5 +1,17 @@
 package service;
 
-public class RegistrationService {
-    
+import model.RegistrationDetail;
+import model.Registration;
+
+public interface RegistrationService {
+
+    /**
+     * Thêm một môn học mới vào Phiếu đăng ký của sinh viên
+     */
+    void registerCourse(RegistrationDetail registrationDetail, Registration newDetail) throws Exception;
+
+    /**
+     * Hủy/Xóa một môn học khỏi Phiếu đăng ký dựa vào mã môn học (courseId)
+     */
+    void cancelCourse(RegistrationDetail registrationDetail, String courseId) throws Exception;
 }
